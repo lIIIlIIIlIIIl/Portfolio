ed38cbf1613bfe27e57a94275cca228a565b8ad4
ShinDongJae <sdhsdj6450@gmail.com>
2022-11-01 17:52:35 +0900

    feat: css theme

--- styles/styled.d.ts
+++ styles/styled.d.ts
@@ -1,5 +1,45 @@
-import 'styled-components';
+import "styled-components";
 
-declare module 'styled-components' {
-  export interface DefaultTheme {}
+declare module "styled-components" {
+  export interface DefaultTheme {
+    breakPoints: {
+      mobile: string;
+      tablet: string;
+      desktop: string;
+    };
+    colors: {
+      teal50: string;
+      teal100: string;
+      teal200: string;
+      teal400: string;
+      teal500: string;
+      teal600: string;
+      teal700: string;
+
+      black100: string;
+      black200: string;
+      black300: string;
+      black400: string;
+      black500: string;
+      black900: string;
+      black000: string;
+
+      red500: string;
+      red600: string;
+      red700: string;
+
+      white000: string;
+
+      blue: string;
+    };
+    fontSize: {
+      size08: string;
+      size12: string;
+      size15: string;
+      size18: string;
+      size20: string;
+      size24: string;
+      size32: string;
+    };
+  }
 }
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,3 +1,43 @@
-import { DefaultTheme } from 'styled-components';
+import { DefaultTheme } from "styled-components";
 
-export const theme: DefaultTheme = {};
+export const theme: DefaultTheme = {
+  breakPoints: {
+    mobile: "481px",
+    tablet: "769px",
+    desktop: "1024px",
+  },
+  colors: {
+    teal50: "#E0F7FA",
+    teal100: "#A7FFEB",
+    teal200: "#64FFDA",
+    teal400: "#1DE9B6",
+    teal500: "#00BCD4",
+    teal600: "#00ACC1",
+    teal700: "#00BFA5",
+
+    black100: "#F5F5F5",
+    black200: "#EEEEEE",
+    black300: "#E0E0E0",
+    black400: "#BDBDBD",
+    black500: "#9E9E9E",
+    black900: "#212121",
+    black000: "#000000",
+
+    red500: "#F44336",
+    red600: "#E53935",
+    red700: "#D50000",
+
+    white000: "#ffffff",
+
+    blue: "#1890ff",
+  },
+  fontSize: {
+    size08: "8px",
+    size12: "12px",
+    size15: "15px",
+    size18: "18px",
+    size20: "20px",
+    size24: "24px",
+    size32: "32px",
+  },
+};
