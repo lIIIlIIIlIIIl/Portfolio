a8db9c30c7f3ad162e0ee42df33a0089930cc9d3
ShinDongJae <sdhsdj6450@gmail.com>
2022-11-05 00:00:13 +0900

    feat: CSS Font

--- styles/global-style.ts
+++ styles/global-style.ts
@@ -11,6 +11,7 @@ html, body{
 
 * {
   box-sizing: border-box;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 
 ol, ul, li{
