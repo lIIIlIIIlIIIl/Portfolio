9ef361d4bb8afe179a4d255ec53e45a69f2b41c0
ShinDongJae <sdhsdj6450@gmail.com>
2022-11-10 17:37:51 +0900

    fix: meta-img

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,7 +43,7 @@ export default class MyDocument extends Document {
             property="og:description"
             content="사용자 경험에 민감하고 소통을 중요하게 생각하는 개발자 신동재입니다. 새로운 기술과 트랜드를 익히고 도전하는 것을 좋아합니다. 어제보다 나은 사람이 되기 위해 계속해서 공부하고 있습니다."
           />
-          <meta property="og:image" content="/Project/02.png" />
+          <meta property="og:image" content="/logo02.png" />
           <link rel="icon" href="/logo01.ico" />
         </Head>
         <body>
